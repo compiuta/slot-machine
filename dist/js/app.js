c0bb105c3f43e1da543e059deec46da0feb29fbf
compiuta <christianr@rocketmail.com>
2020-05-18 11:50:24 -0700

    current build of site

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,3 +1,39 @@
-console.log('model initialized');
-console.log('controller initialized');
-console.log('view initialized');
+(function (window) {
+    'use strict';
+
+    console.log('model initialzed');
+
+    const slotMachineModel = {
+
+    }
+
+    window.app = window.app || {};
+    window.app.slotMachineModel = slotMachineModel;
+})(window);
+(function (window) {
+    'use strict';
+
+    console.log('controller initialized');
+
+    const slotMachineController = {
+
+    }
+
+    window.app = window.app || {};
+    window.app.slotMachineController = slotMachineController;
+})(window);
+(function (window) {
+    'use strict';
+
+    const slotReels = document.querySelectorAll('[data-reel]');
+    const slotStartButton = document.querySelector('[data-slot="startButton"]');
+
+    console.log('view initialized');
+
+    const slotMachineView = {
+
+    }
+
+    window.app = window.app || {};
+    window.app.slotMachineView = slotMachineView;
+})(window);
