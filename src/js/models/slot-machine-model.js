7103fa0763f51cf6365175d214042bc61a0d6cb1
compiuta <christianr@rocketmail.com>
2020-05-20 10:01:21 -0700

    add default data and get data function

--- src/js/models/slot-machine-model.js
+++ src/js/models/slot-machine-model.js
@@ -1,10 +1,47 @@
 (function (window) {
     'use strict';
 
+    const defaultSlotData = [
+        {
+            symbol: 'diamond',
+            value: 800,
+            frequency: 2
+        },
+        {
+            symbol: 'seven',
+            value: 80,
+            frequency: 4
+        },
+        {
+            symbol: 'triple-bar',
+            value: 40,
+            frequency: 6
+        },
+        {
+            symbol: 'double-bar',
+            value: 25,
+            frequency: 8
+        },
+        {
+            symbol: 'single-bar',
+            value: 10,
+            frequency: 10
+        },
+        {
+            symbol: 'cherry',
+            value: 10,
+            frequency: 12
+        }
+    ];
+
     console.log('model initialzed');
 
     const slotMachineModel = {
-
+        getData: function (stateRequested) {
+            if (stateRequested === 'default') {
+                return defaultSlotData;
+            }
+        }
     }
 
     window.app = window.app || {};
