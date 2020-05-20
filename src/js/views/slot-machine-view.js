fcfed8938f3db87a2017f3b1e90cc7a21ac645b0
compiuta <christianr@rocketmail.com>
2020-05-20 10:03:19 -0700

    add populate reels function

--- src/js/views/slot-machine-view.js
+++ src/js/views/slot-machine-view.js
@@ -4,10 +4,19 @@
     const slotReels = document.querySelectorAll('[data-reel]');
     const slotStartButton = document.querySelector('[data-slot="startButton"]');
 
+    const currentData = app.slotMachineController.getCurrentData('default');
+
+    function populateSlotReels(data) {
+        slotReels.forEach(function (reel) {
+            reel.innerText = JSON.stringify(data);
+        });
+    }
+
+    populateSlotReels(currentData);
+
     console.log('view initialized');
 
     const slotMachineView = {
-
     }
 
     window.app = window.app || {};
