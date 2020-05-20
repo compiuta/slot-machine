58090627af8757bbf39b160e496d172b479a3450
compiuta <christianr@rocketmail.com>
2020-05-20 10:02:12 -0700

    add getcurrentdata function

--- src/js/controllers/slot-machine-controller.js
+++ src/js/controllers/slot-machine-controller.js
@@ -1,10 +1,22 @@
 (function (window) {
     'use strict';
 
+    function evaluateRow() {
+
+    }
+
+    function getCurrentData(dataType) {
+        const dataRequested = app.slotMachineModel.getData(dataType);
+        return dataRequested;
+    }
+
     console.log('controller initialized');
 
     const slotMachineController = {
-
+        getCurrentData: function (dataType) {
+            const dataRequested = getCurrentData(dataType);
+            return dataRequested;
+        }
     }
 
     window.app = window.app || {};
